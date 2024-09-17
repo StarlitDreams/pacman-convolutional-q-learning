bf3c786982eadb70574c895251942038657b44e6
Lena Ayen <hari020204@gmail.com>
2024-09-17 19:37:43 +0530

    set up propagation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader, TensorDataset
 
 
 class Network(nn.Module):
-    def __init__(self,action_size, seed=42):
+    def __init__(self,action_size, seed=42) -> None:
         super(Network,self).__init__()
         self.seed = torch.manual_seed(seed)
         self.conv1=nn.Conv2d(3,32,kernel_size=8,stride=4)
@@ -25,6 +25,14 @@ class Network(nn.Module):
         self.fc2=nn.Linear(512,256)
         self.fc3=nn.Linear(256,action_size)
         
-        
-        
-        
+    def forward(self,state) -> torch.Tensor:
+        x=F.relu(self.bn1(self.conv1(state)))
+        x=F.relu(self.bn2(self.conv2(x)))
+        x=F.relu(self.bn3(self.conv3(x)))        
+        x=F.relu(self.bn4(self.conv4(x)))
+        x=x.view(x.size(0),-1)
+        x=F.relu(self.fc1(x))
+        x=F.relu(self.fc2(x))
+        return self.fc3(x)
+    
+    
