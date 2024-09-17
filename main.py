541bebe0c11265ffc4e715a7606c37729faafe98
Lena Ayen <hari020204@gmail.com>
2024-09-17 18:54:39 +0530

    built the network class

---
+++ main.py
@@ -0,0 +1,30 @@
+import os 
+import random
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from collections import deque
+from torch.utils.data import DataLoader, TensorDataset
+
+
+class Network(nn.Module):
+    def __init__(self,action_size, seed=42):
+        super(Network,self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.conv1=nn.Conv2d(3,32,kernel_size=8,stride=4)
+        self.bn1=nn.BatchNorm2d(32)
+        self.conv2=nn.Conv2d(32,64,kernel_size=4,stride=2)
+        self.bn2=nn.BatchNorm2d(64)
+        self.conv3=nn.Conv2d(64,64,kernel_size=3,stride=1)
+        self.bn3=nn.BatchNorm2d(64)
+        self.conv4=nn.Conv2d(64,128,kernel_size=3,stride=1)
+        self.bn4=nn.BatchNorm2d(128)
+        self.fc1=nn.Linear(128*10*10,512)
+        self.fc2=nn.Linear(512,256)
+        self.fc3=nn.Linear(256,action_size)
+        
+        
+        
+        
