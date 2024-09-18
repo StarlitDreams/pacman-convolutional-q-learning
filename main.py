07f50de8cbccf2e34c6b4168590a826e8f2279b7
Lena Ayen <hari020204@gmail.com>
2024-09-18 15:05:42 +0530

    built the agent class

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 from collections import deque
 from torch.utils.data import DataLoader, TensorDataset
+from PIL import Image
+from torchvision import transforms
+
 
 
 class Network(nn.Module):
@@ -35,4 +38,62 @@ class Network(nn.Module):
         x=F.relu(self.fc2(x))
         return self.fc3(x)
     
-    
+learning_rate = 1e-3 
+batch_size = 256     
+gamma = 0.99
+
+def preprocess_frame(frame):
+    frame = Image.fromarray(frame)
+    preprocess=transforms.Compose([transforms.Resize((128,128)),transforms.ToTensor()])
+    return preprocess(frame).unsqueeze(0)
+ 
+
+class Agent():
+    def __init__(self, action_size):
+        self.device = torch.device("cuda:0")
+        self.action_size = action_size
+        self.local_qnetwork = Network( action_size).to(self.device)
+        self.target_qnetwork = Network( action_size).to(self.device)
+        self.optimizer = optim.Adam(self.local_qnetwork.parameters(), lr=learning_rate)
+        self.memory = deque(maxlen=int(1e5))
+        self.criterion = nn.SmoothL1Loss()  
+        self.scaler = torch.amp.GradScaler("cuda:0") 
+
+    def step(self, state, action, reward, next_state, done):
+        state = preprocess_frame(state)
+        next_state = preprocess_frame(next_state)
+        self.memory.append((state, action, reward, next_state, done))
+        if len(self.memory) > batch_size:
+            experiences = random.sample(self.memory, batch_size)
+            self.learn(experiences, gamma)
+
+    def act(self, state, epsilon=0.0):
+        state = preprocess_frame(state).to(self.device)
+        self.local_qnetwork.eval()
+        with torch.no_grad():
+            action_values = self.local_qnetwork(state)
+        self.local_qnetwork.train()
+        if random.random() > epsilon:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def learn(self, experiences, gamma):
+        states, actions, rewards,next_states, dones = zip(*experiences)
+        idx = np.random.choice(self.size, batch_size, replace=False)
+        states = torch.from_numpy(self.states[idx]).float().to(self.device)
+        next_states = torch.from_numpy(self.next_states[idx]).float().to(self.device)
+        actions = torch.from_numpy(self.actions[idx]).long().to(self.device)
+        rewards = torch.from_numpy(self.rewards[idx]).float().to(self.device)
+        dones = torch.from_numpy(self.dones[idx]).float().to(self.device)
+        with torch.amp.autocast("cuda:0"): 
+            next_q_targets = self.target_qnetwork(next_states).detach().max(1)[0].unsqueeze(1)
+            q_targets = rewards + (gamma * next_q_targets * (1 - dones))
+            q_expected = self.local_qnetwork(states).gather(1, actions)
+            loss = self.criterion(q_expected, q_targets)
+        self.optimizer.zero_grad()
+        self.scaler.scale(loss).backward()
+        self.scaler.step(self.optimizer)
+        self.scaler.update()
+
+ 
